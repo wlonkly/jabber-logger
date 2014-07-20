4b640a53316a4247f1c2b9b9d7fb7812411b8b39
Rich Lafferty <rich@freshbooks.com>
2014-07-20 16:11:42 -0400

    globals; apply filters and excludes

--- lib/logtail.rb
+++ lib/logtail.rb
@@ -1,24 +1,38 @@
 # logtail.rb: log-tailing part of falebot-ruby
 
-# explicitly disable sincedb, independent runs should be completely independent.
-# i'm not sure if i've missed mysterious implications of this but it APPEARS that
-# it's kept in memory and just cached to the sincedb file at intervals.
-
-# TODO: split into own file and make filters configurable
 def process_line (path, line)
-    return "#{path} #{line}"
+    c = $config['logfiles'][path]
+
+    c['filter'].each do |filter|
+        puts filter['replace']
+        line.gsub!(Regexp.new(filter['replace']), filter['with'])
+    end
+
+    c['exclude'].each do |exclude|
+        # TODO: precompile regexps on startup
+        if Regexp.new(exclude).match(line)
+            return nil
+        end
+    end
+
+    return line
 end
 
-def tail_logs (config, muc)
+def tail_logs_forever 
+    # explicitly disable sincedb, independent runs should be completely independent.
+    # i'm not sure if i've missed mysterious implications of this but it APPEARS that
+    # it's kept in memory and just cached to the sincedb file at intervals.
     t = FileWatch::Tail.new(:stat_interval => 1, :sincedb_path => '/dev/null')
 
-    config['logfiles'].keys.each do |path|
+    $config['logfiles'].keys.each do |path|
         t.tail(path)
     end 
 
     t.subscribe do |path, line|
         l = process_line(path, line)
-        muc.say(l)
+        if l 
+            $muc.say(l)
+        end
     end
 end 
 
