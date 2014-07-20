3b8d481afc41163469b9a71f62a2f6458c718393
Rich Lafferty <rich@freshbooks.com>
2014-07-20 15:41:43 -0400

    Use config file, fewer globals

--- falebot.rb
+++ falebot.rb
@@ -3,24 +3,21 @@ require "rubygems"
 require 'xmpp4r'
 require 'xmpp4r/muc/helper/simplemucclient'
 require "filewatch/tail"
-
-# TODO: separate yaml(?) config
-
-# jabber server credentials
-$jid  = 'test3@jabber.freshbooks.com'
-$pass = 'testing'
-
-# groupchat details
-$room = 'fale2@conference.jabber.freshbooks.com'
-$nick = 'fale2'
-$chatpass = ''
-
-def debug (message)
-  time = Time.now.strftime("%H:%M:%S")
-  puts "[" + time + "] " + message
-end
+require 'yaml'
 
 # TODO: these go in lib/? how does one package a "program" with its supporting
 # files in Ruby anyhow?
 require './jabberclient.rb'
 require './logtail.rb'
+
+# TODO: separate yaml(?) config
+config = YAML.load_file("./config.yml")
+
+muc = jabber_connect(config)
+tail_logs(config, muc)
+
+# def debug (message)
+#   time = Time.now.strftime("%H:%M:%S")
+#  puts "[" + time + "] " + message
+# end
+
--- jabberclient.rb
+++ jabberclient.rb
@@ -3,17 +3,14 @@
 # set up global jabber connection
 #
 
-# TODO: debug config flag
-Jabber::debug = true
-cl = Jabber::Client.new(Jabber::JID.new($jid))
-cl.connect
-cl.auth($pass)
+def jabber_connect (config)
+    Jabber::debug = config['debug']
+    cl = Jabber::Client.new(Jabber::JID.new(config['jabber']['jid']))
+    cl.connect
+    cl.auth(config['jabber']['password'])
 
-$m = Jabber::MUC::SimpleMUCClient.new(cl)
-$m.join($room + "/" + $nick, $chatpass)
-
-$m.on_message { |time,nick,text|
-    if(time == nil)
-       debug("<#{nick}> #{text}")
-    end
-}
+    m = Jabber::MUC::SimpleMUCClient.new(cl)
+    m.join(config['jabber']['muc_room'] + "/" + config['jabber']['muc_nick'], 
+           config['jabber']['muc_password'])
+    return m
+end
