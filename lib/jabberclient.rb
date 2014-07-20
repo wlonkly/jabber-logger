a7c0e2cd6f469bc8e32fb21524c5f68a42177c9c
Rich Lafferty <rich@freshbooks.com>
2014-07-20 16:11:17 -0400

    give in and use globals

--- lib/jabberclient.rb
+++ lib/jabberclient.rb
@@ -3,14 +3,14 @@
 # set up global jabber connection
 #
 
-def jabber_connect (config)
-    Jabber::debug = config['debug']
-    cl = Jabber::Client.new(Jabber::JID.new(config['jabber']['jid']))
+def connect_jabber_muc 
+    Jabber::debug = $config['debug']
+    cl = Jabber::Client.new(Jabber::JID.new($config['jabber']['jid']))
     cl.connect
-    cl.auth(config['jabber']['password'])
+    cl.auth($config['jabber']['password'])
 
     m = Jabber::MUC::SimpleMUCClient.new(cl)
-    m.join(config['jabber']['muc_room'] + "/" + config['jabber']['muc_nick'], 
-           config['jabber']['muc_password'])
+    m.join($config['jabber']['muc_room'] + "/" + $config['jabber']['muc_nick'], 
+           $config['jabber']['muc_password'])
     return m
 end
