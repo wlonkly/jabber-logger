2f2058160d3f87516436f3bfe6495483f257762a
Rich Lafferty <rich@freshbooks.com>
2014-07-21 20:08:57 -0400

    Logging

--- lib/jabberclient.rb
+++ lib/jabberclient.rb
@@ -3,8 +3,9 @@
 # set up global jabber connection
 #
 
-def connect_jabber_muc 
-    Jabber::debug = $config['debug']
+def connect_jabber_muc
+    $logger.info "Connecting to Jabber..."
+    Jabber::debug = $config['jabber']['debug'] || false
     cl = Jabber::Client.new(Jabber::JID.new($config['jabber']['jid']))
     cl.connect
     cl.auth($config['jabber']['password'])
@@ -12,5 +13,6 @@ def connect_jabber_muc
     m = Jabber::MUC::SimpleMUCClient.new(cl)
     m.join($config['jabber']['muc_room'] + "/" + $config['jabber']['muc_nick'], 
            $config['jabber']['muc_password'])
+    $logger.info "Connected."
     return m
 end
--- lib/logtail.rb
+++ lib/logtail.rb
@@ -1,12 +1,15 @@
 # logtail.rb: log-tailing part of falebot-ruby
 
 def process_line (path, line)
+    $logger.debug "Pre:  #{path}: #{line}"
+
     c = $config['logfiles'][path]
 
     excludes = c['exclude'] || []
     excludes.each do |exclude|
         # TODO: precompile regexps on startup
         if Regexp.new(exclude).match(line)
+            logger.debug "Line matches /#{exclude}/, excluding"
             return nil
         end
     end
@@ -15,6 +18,8 @@ def process_line (path, line)
     filters.each do |filter|
         line.gsub!(Regexp.new(filter['replace']), filter['with'])
     end
+    $logger.debug "Post: #{path}: #{line}"
+
     line
 end
 
@@ -25,6 +30,7 @@ def tail_logs_forever
     t = FileWatch::Tail.new(:stat_interval => 1, :sincedb_path => '/dev/null')
 
     $config['logfiles'].keys.each do |path|
+        $logger.info "Started watching #{path}"
         t.tail(path)
     end 
 
