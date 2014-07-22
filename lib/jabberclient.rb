300b1aa6af9ae53b2bc66c1256f4470011769e55
Rich Lafferty <rich@freshbooks.com>
2014-07-21 20:10:37 -0400

    two-space tabs

--- lib/jabberclient.rb
+++ lib/jabberclient.rb
@@ -4,15 +4,15 @@
 #
 
 def connect_jabber_muc
-    $logger.info "Connecting to Jabber..."
-    Jabber::debug = $config['jabber']['debug'] || false
-    cl = Jabber::Client.new(Jabber::JID.new($config['jabber']['jid']))
-    cl.connect
-    cl.auth($config['jabber']['password'])
+  $logger.info "Connecting to Jabber..."
+  Jabber::debug = $config['jabber']['debug'] || false
+  cl = Jabber::Client.new(Jabber::JID.new($config['jabber']['jid']))
+  cl.connect
+  cl.auth($config['jabber']['password'])
 
-    m = Jabber::MUC::SimpleMUCClient.new(cl)
-    m.join($config['jabber']['muc_room'] + "/" + $config['jabber']['muc_nick'], 
-           $config['jabber']['muc_password'])
-    $logger.info "Connected."
-    return m
+  m = Jabber::MUC::SimpleMUCClient.new(cl)
+  m.join($config['jabber']['muc_room'] + "/" + $config['jabber']['muc_nick'], 
+      $config['jabber']['muc_password'])
+  $logger.info "Connected."
+  return m
 end
--- lib/logtail.rb
+++ lib/logtail.rb
@@ -1,44 +1,44 @@
 # logtail.rb: log-tailing part of falebot-ruby
 
 def process_line (path, line)
-    $logger.debug "Pre:  #{path}: #{line}"
+  $logger.debug "Pre:  #{path}: #{line}"
 
-    c = $config['logfiles'][path]
+  c = $config['logfiles'][path]
 
-    excludes = c['exclude'] || []
-    excludes.each do |exclude|
-        # TODO: precompile regexps on startup
-        if Regexp.new(exclude).match(line)
-            logger.debug "Line matches /#{exclude}/, excluding"
-            return nil
-        end
+  excludes = c['exclude'] || []
+  excludes.each do |exclude|
+    # TODO: precompile regexps on startup
+    if Regexp.new(exclude).match(line)
+      logger.debug "Line matches /#{exclude}/, excluding"
+      return nil
     end
-    
-    filters =  c['filter'] || []     
-    filters.each do |filter|
-        line.gsub!(Regexp.new(filter['replace']), filter['with'])
-    end
-    $logger.debug "Post: #{path}: #{line}"
+  end
+  
+  filters =  c['filter'] || []     
+  filters.each do |filter|
+    line.gsub!(Regexp.new(filter['replace']), filter['with'])
+  end
+  $logger.debug "Post: #{path}: #{line}"
 
-    line
+  line
 end
 
 def tail_logs_forever 
-    # explicitly disable sincedb, independent runs should be completely independent.
-    # i'm not sure if i've missed mysterious implications of this but it APPEARS that
-    # it's kept in memory and just cached to the sincedb file at intervals.
-    t = FileWatch::Tail.new(:stat_interval => 1, :sincedb_path => '/dev/null')
+  # explicitly disable sincedb, independent runs should be completely independent.
+  # i'm not sure if i've missed mysterious implications of this but it APPEARS that
+  # it's kept in memory and just cached to the sincedb file at intervals.
+  t = FileWatch::Tail.new(:stat_interval => 1, :sincedb_path => '/dev/null')
 
-    $config['logfiles'].keys.each do |path|
-        $logger.info "Started watching #{path}"
-        t.tail(path)
-    end 
+  $config['logfiles'].keys.each do |path|
+    $logger.info "Started watching #{path}"
+    t.tail(path)
+  end 
 
-    t.subscribe do |path, line|
-        l = process_line(path, line)
-        if l 
-            $muc.say(l)
-        end
+  t.subscribe do |path, line|
+    l = process_line(path, line)
+    if l 
+      $muc.say(l)
     end
+  end
 end 
 
